e34819f0b14bf75d0661a13130bfb458a67e0623
aeschylus <scipioaffricanus@gmail.com>
2013-08-24 01:09:47 -0700

    getTrial modified to get the Title & URL

--- getTrial.js
+++ getTrial.js
@@ -3,18 +3,21 @@
 // xhr.send();
 
 var link = document.URL;
-chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-   function(tabs){
-      console.log(tabs[0]);
+chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}, function(tabs){
+    	var currentTab = tabs[0];
+		$.post("http://192.168.1.130:3000",
+		  {
+		  	user_url: currentTab.url, 
+		  	user_pagetitle: currentTab.title,
+		  	console.log(user_url),
+		  	console.log(user_pagetitle),
+		  },
+		  function(works){
+		    alert("This works!!");
+		  });
+
    }
 );
 
-$.post("http://192.168.1.130:3000",
-  {
-  	user_post:link,
-  },
-  function(works){
-    alert("This works!!");
-  });
 
 
